436eb77a1da56388d6c016f6f2d3f5bc634293a9
tristanfrench <winstanley.tristan@gmail.com>
2020-07-20 22:27:32 +0200

    Spawn bullets

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    //Todo move bullets, delete bullets that have travelled a certain distance
+    [SerializeField] private Transform _prefab;
+    private Rigidbody2D Blob;
+    private int _bulletCount;
+    void Start()
+    {
+        Blob = GetComponent<Rigidbody2D>();
+        _bulletCount = 0;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(_prefab, Blob.position, Quaternion.identity);
+            _bulletCount++;
+        }
+
+    }
+}
