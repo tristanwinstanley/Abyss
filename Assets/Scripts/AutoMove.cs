c611cf5a63389f377beac90bdc57fe3805772179
tristanfrench <winstanley.tristan@gmail.com>
2020-08-06 21:50:00 +0200

    Bullets now move, need to clean up + put apply velocity method into single external class

---
+++ Assets/Scripts/AutoMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoMove : MonoBehaviour
+{
+    private Rigidbody2D bullet;
+    private Transform bulletTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bullet = GetComponent<Rigidbody2D>();
+        bulletTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bulletTransform.position += new Vector3(0.2f, 0.2f);
+        //ApplyVelocity(bullet, 10, 10);
+    }
+    private void ApplyVelocity(Rigidbody2D rb, float x = 0f, float y = 0f)
+    {
+        // if x or y is 0, use previous velocity, i.e don't change
+        //if (x == 0f)
+        //    x = rb.velocity.x;
+        //if (y == 0f)
+        //    y = rb.velocity.y;
+
+        rb.velocity = new Vector2(x, y);
+    }
+}
--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Shoot : MonoBehaviour
 {
     //Todo move bullets, delete bullets that have travelled a certain distance
-    [SerializeField] private Transform _prefab;
+    [SerializeField] private GameObject _prefab;
     private Rigidbody2D Blob;
     private int _bulletCount;
     void Start()
@@ -17,11 +17,31 @@ public class Shoot : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        GameObject bullet = null;
         if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(_prefab, Blob.position, Quaternion.identity);
+            bullet = Instantiate(_prefab, Blob.position, Quaternion.identity);
+            
+            Rigidbody2D bulletRB = _prefab.GetComponent<Rigidbody2D>();
+            //currently applying velocity does not change anything..
+            //ApplyVelocity(bulletRB, 100, 100);
             _bulletCount++;
         }
 
+        if (bullet != null)
+        {
+            bullet.transform.position += new Vector3(10, 10);
+        }
+
+    }
+    private void ApplyVelocity(Rigidbody2D rb, float x = 0f, float y = 0f)
+    {
+        // if x or y is 0, use previous velocity, i.e don't change
+        //if (x == 0f)
+        //    x = rb.velocity.x;
+        //if (y == 0f)
+        //    y = rb.velocity.y;
+
+        rb.velocity = new Vector2(x, y);
     }
 }
