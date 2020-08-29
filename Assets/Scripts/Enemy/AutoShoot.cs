ee13574a53917b4733d025c2f5f1da26ef9e4206
tristanfrench <winstanley.tristan@gmail.com>
2020-08-29 22:22:30 +0200

    Enemy shoots automatically every .5s

--- Assets/Scripts/Enemy/AutoShoot.cs
+++ Assets/Scripts/Enemy/AutoShoot.cs
@@ -6,23 +6,25 @@ namespace Assets.Scripts.Enemy
     {
         Transform CurrentTransform;
         [SerializeField] private GameObject projectilePrefab;
+        float timeSinceLastShot;
         void Start()
         {
             CurrentTransform = GetComponent<Transform>();
+            timeSinceLastShot = Time.realtimeSinceStartup;
         }
 
 
         // Update is called once per frame
         void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Time.realtimeSinceStartup - timeSinceLastShot > 0.5f)
             {
+                timeSinceLastShot = Time.realtimeSinceStartup;
                 //Normalized vector between Current transform and player
                 Vector2 projectileVelocityVector = Utils.Get2DNormVector(GameObject.FindWithTag(Constants.PLAYER_TAG).transform.position - CurrentTransform.position);
                 //Shoot projectile from current position with specific direction and speed
                 Shoot(transform.position, projectileVelocityVector * Constants.BULLET_SPEED);
             }
-
         }
         private void Shoot(Vector2 position, Vector2 velocity)
         {
