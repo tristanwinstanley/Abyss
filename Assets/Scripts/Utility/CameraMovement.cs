dbc473ba2a1888e8b7b3811a8119376dcec9fcf0
tristanfrench <winstanley.tristan@gmail.com>
2021-06-15 22:00:51 +0200

    Small refactoring

--- Assets/Scripts/Utility/CameraMovement.cs
+++ Assets/Scripts/Utility/CameraMovement.cs
@@ -4,9 +4,13 @@ namespace Assets.Scripts.Utility
 {
     public class CameraMovement : MonoBehaviour
     {
+        private GameObject player;
+        private void Start()
+        {
+            player = GameObject.FindWithTag("Player");
+        }
         private void FixedUpdate()
         {
-            GameObject player = GameObject.FindWithTag("Player");
             transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
         }
     }
