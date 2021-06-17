81e83a4ee6ab982e9687f9757f39ed7536b24460
tristanfrench <winstanley.tristan@gmail.com>
2021-06-17 22:52:34 +0200

    Created EntitySO

---
+++ Assets/Scripts/Entities/BaseEntityScripts/EntityData.cs
@@ -0,0 +1,15 @@
+﻿using Assets.Scripts.ScriptableObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Entities.BaseEntityScripts
+{
+    public class EntityData : MonoBehaviour
+    {
+        [SerializeField] private EntitySO entitySO;
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/EntitySO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "New Entity SO", menuName = "EntitySO")]
+    public class EntitySO : ScriptableObject
+    {
+        public float Health;
+        public int Damage;
+        public int Speed;
+    }
+}
